d7df00563971fc9a6e7d356ec5ebab472582b37a
Andreea Calin <calinmadalina30@gmail.com>
2021-11-20 16:35:40 +0200

    Updated app.modules

--- loneliness-app/src/app/app.module.ts
+++ loneliness-app/src/app/app.module.ts
@@ -11,6 +11,20 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import { JwPaginationModule } from 'jw-angular-pagination';
 
 @NgModule({
   declarations: [
@@ -26,7 +40,24 @@ import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MDBBootstrapModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatSelectModule,
+    AmplifyUIAngularModule,
+    JwPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent],
